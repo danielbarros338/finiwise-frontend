439c3d488b941fd1eb62e5d7a73d53aeb70d9695
Daniel <usrrufus12@omint.com.br>
2024-09-11 18:52:54 -0300

    feat: instaled apexcharts

---
+++ src/components/home/EarningsCardComponent.vue
@@ -0,0 +1,15 @@
+<template>
+  <q-card>
+    <q-card-section class="text-h6">Ganhos</q-card-section>
+
+    <q-separator inset />
+
+    <q-card-section> teste </q-card-section>
+  </q-card>
+</template>
+
+<script setup lang="ts">
+defineOptions({
+  name: 'EarningsCardComponent',
+});
+</script>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,8 +1,16 @@
 <template>
-  <q-page class="row"> </q-page>
+  <q-page class="q-pa-md bg-fw-primary-light">
+    <div class="row">
+      <div class="col-xs-12">
+        <EarningCardComponent />
+      </div>
+    </div>
+  </q-page>
 </template>
 
 <script setup lang="ts">
+import EarningCardComponent from '/src/components/home/EarningsCardComponent.vue';
+
 defineOptions({
   name: 'IndexPage',
 });
