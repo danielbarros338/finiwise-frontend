7ca92fc513d166a66e8dba3368a9a56374f06aef
Daniel <danielbarros338@gmail.com>
2025-03-15 19:31:54 -0300

    created structure of earning session

--- src/pages/earnings/IndexPage.vue
+++ src/pages/earnings/IndexPage.vue
@@ -10,11 +10,23 @@
     </CardBase>
 
     <q-btn class="btn q-mt-md bg-fw-button-primary text-white" dense>Adicionar ganho</q-btn>
+
+    <CardBase class="bg-fw-blue-light q-mt-md"> SELECIONAR CARTEIRA </CardBase>
+
+    <CardBase class="bg-fw-primary q-mt-md">
+      <span class="text-h6 text-white">Ganhos do mês MÊS</span>
+      <DataComponent class="bg-fw-blue-light" />
+    </CardBase>
+
+    <CardBase class="bg-fw-blue-light q-mt-md"> Relatórios Gráficos </CardBase>
+
+    <CardBase class="bg-fw-blue-light q-mt-md"> Relatórios LLM </CardBase>
   </q-page>
 </template>
 
 <script setup lang="ts">
 import CardBase from 'src/components/shared/CardBase.vue'
+import DataComponent from 'src/components/shared/DataComponent.vue'
 </script>
 
 <style scoped>
